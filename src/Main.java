0074854fd28e442867562e71325149a2d34477eb
Vitaly-IS-23 <vitalie01rus01@gmail.com>
2024-11-09 22:47:10 +0300

    - В задаче 2 методы поиска минимума и максимума заменены на циклы - В задаче 4 убран алгоритм разворота массива и заменен на обычный вывод элементов массива

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,20 @@ public class Main {
         System.out.printf("Сумма трат за месяц составила %.2f рублей", totalSpending);
 
         //Задача 2
-        double maxSpending = Arrays.stream(spendingMoney).max().getAsDouble();
-        double minSpending = Arrays.stream(spendingMoney).min().getAsDouble();
+        int maxSpendingIndex = 0;
+        int minSpendingIndex = 0;
 
-        System.out.printf("\n\nМинимальная сумма трат за неделю составила %f рублей. Максимальная сумма трат за неделю составила %f рублей", minSpending, maxSpending);
+        for (int i = 1; i < spendingMoney.length; i++) {
+            if (spendingMoney[i] > spendingMoney[maxSpendingIndex]){
+                maxSpendingIndex = i;
+            }
+            if (spendingMoney[i] < spendingMoney[minSpendingIndex]){
+                minSpendingIndex = i;
+            }
+        }
+
+        System.out.printf("\n\nМинимальная сумма трат за неделю составила %f рублей. Максимальная сумма трат за неделю составила %f рублей",
+                spendingMoney[minSpendingIndex], spendingMoney[maxSpendingIndex]);
 
         //Задача 3
         double averageSpending = Arrays.stream(spendingMoney).average().getAsDouble();
@@ -27,14 +37,8 @@ public class Main {
         System.out.println("\n");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
 
-        for (int i = 0; i < reverseFullName.length / 2; i++) {
-            char temp = reverseFullName[reverseFullName.length - 1 - i];
-            reverseFullName[reverseFullName.length - 1 - i] = reverseFullName[i];
-            reverseFullName[i] = temp;
-        }
-
-        for (char c : reverseFullName){
-            System.out.print(c);
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
 }
